58dba029f6775092b580891fd3b9b23a008a1c10
Hans Buker <hansbuker1993@gmail.com>
2023-03-28 14:47:02 -0300

    sp: minimal changes before deploy

--- src/routes/Employees.router.ts
+++ src/routes/Employees.router.ts
@@ -1,9 +1,8 @@
 import { Router } from "express"
-import { postEmployee, updateEmployeeController } from "../controllers/Employees.controller";
+import { postEmployee } from "../controllers/Employees.controller";
 
 const routerEmployee = Router();
 
 routerEmployee.post('/employee/post', postEmployee);
-routerEmployee.post('/employee/update/:id', updateEmployeeController)
 
 export { routerEmployee }
